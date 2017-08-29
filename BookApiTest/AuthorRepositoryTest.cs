2c4cc6e00b2b479ea5e9bf79d3fd172644e64071
Mangek <magnus.kjellander@valtech.se>
2017-08-29 14:57:55 +0200

    Added tests

---
+++ BookApiTest/AuthorRepositoryTest.cs
@@ -0,0 +1,80 @@
+﻿using BookApi.Models;
+using BookApi.Repository;
+using NUnit.Framework;
+
+namespace BookApiTest
+{
+    [TestFixture]
+    public class AuthorRepositoryTest
+    {
+        private AuthorRepository _authorRepository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _authorRepository = new AuthorRepository();
+        }
+
+        [Test]
+        public void Should_return_null_if_author_doesnt_exist()
+        {
+            var author = _authorRepository.Find(10);
+
+            Assert.That(author, Is.Null);
+        }
+
+        [Test]
+        public void Should_add_new_author()
+        {
+            var newAuthor = new Author
+            {
+                Id = 2,
+                Name = "Jens Lapidus"
+            };
+
+            _authorRepository.Create(newAuthor);
+            var author = _authorRepository.Find(newAuthor.Id);
+
+            Assert.That(author, Is.EqualTo(author));
+        }
+
+        [Test]
+        public void Should_update_author()
+        {
+            var newAuthor = new Author
+            {
+                Id = 2,
+                Name = "Jens Lapidus"
+            };
+            var updateAuthor = new Author
+            {
+                Id = 2,
+                Name = "Hulk Hogan"
+            };
+
+            _authorRepository.Create(newAuthor);
+            _authorRepository.Update(newAuthor.Id, updateAuthor);
+
+            var author = _authorRepository.Find(newAuthor.Id);
+
+            Assert.That(author, Is.EqualTo(updateAuthor));
+        }
+
+        [Test]
+        public void Should_remove_author()
+        {
+            var newAuthor = new Author
+            {
+                Id = 2,
+                Name = "Jens Lapidus"
+            };
+
+            _authorRepository.Create(newAuthor);
+            _authorRepository.Remove(newAuthor.Id);
+
+            var author = _authorRepository.Find(newAuthor.Id);
+
+            Assert.That(author, Is.Null);
+        }
+    }
+}
---
+++ BookApiTest/BookRepositoryTest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BookApi.Models;
+using BookApi.Repository;
+using NUnit.Framework;
+
+namespace BookApiTest
+{
+    [TestFixture]
+    public class BookRepositoryTest
+    {
+        private BookRepository _bookRepository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _bookRepository = new BookRepository();
+        }
+
+        [Test]
+        public void Should_return_null_if_book_doesnt_exist()
+        {
+            var book = _bookRepository.Find(10);
+
+            Assert.That(book, Is.Null);
+        }
+
+        [Test]
+        public void Should_add_new_book()
+        {
+            var newBook = new Book
+            {
+                Id = 2,
+                Title = "Aldrig fucka upp",
+                Intro = "Uppföljaren till succén...",
+                Author = 1
+            };
+
+            _bookRepository.Create(newBook);
+            var book = _bookRepository.Find(newBook.Id);
+
+            Assert.That(book, Is.EqualTo(book));
+        }
+
+        [Test]
+        public void Should_update_book()
+        {
+            var newBook = new Book
+            {
+                Id = 2,
+                Title = "Aldrig fucka upp",
+                Intro = "Uppföljaren till succén...",
+                Author = 1
+            };
+            var updateBook = new Book
+            {
+                Id = 2,
+                Title = "Sthlm Delete",
+                Intro = "Lapidus nya serie...",
+                Author = 1
+            };
+
+            _bookRepository.Create(newBook);
+            _bookRepository.Update(newBook.Id, updateBook);
+
+            var book = _bookRepository.Find(newBook.Id);
+
+            Assert.That(book, Is.EqualTo(updateBook));
+        }
+
+        [Test]
+        public void Should_remove_book()
+        {
+            var newBook = new Book
+            {
+                Id = 2,
+                Title = "Aldrig fucka upp",
+                Intro = "Uppföljaren till succén...",
+                Author = 1
+            };
+
+            _bookRepository.Create(newBook);
+            _bookRepository.Remove(newBook.Id);
+
+            var book = _bookRepository.Find(newBook.Id);
+
+            Assert.That(book, Is.Null);
+        }
+    }
+}
